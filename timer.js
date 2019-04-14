cf1675ec50d28d157fc5cb30625745de63e230f3
Nick Silvestri <nickpsilvestri@gmail.com>
2019-04-13 19:28:10 -0700

    moved timer toggle to its own function

--- timer.js
+++ timer.js
@@ -17,52 +17,59 @@
         /* set timer state to off */
         timerRunning = false;
 
-        /* Handles spacebar press. Does not use JS intervals or timeout for
-         * timekeeping; those are not guaranteed to be accurate. Instead uses
-         * JS Date().
+        /* Handles spacebar press.
          */
         document.body.onkeyup = function(event) {
             // handles key presses, checks multiple properties for browser compatibility
             if(event.keyCode === 32 || event.key === 'Spacebar'){
-                if (timerRunning) {
-                    // turn timer off
-                    timerRunning = false;
-                    clearInterval(timer);
-                }
-                else {
-                    // turn timer on
-                    timerRunning = true;
-
-                    // get current time
-                    timerStart = Date.now();
-
-                    // set up interval to change timer text
-                    timer = setInterval(function() {
-                        // delta is ms since timer started
-                        let delta = Date.now() - timerStart;
-                        
-                        // get values of numbers for parts of timer 
-                        let minutes = Math.floor(delta / 60000);
-                        let seconds = Math.floor(delta / 1000) % 60; // % 60 is for second rollover when > 1 minute
-                        let ms = Math.floor(delta - (minutes * 60000) - (seconds * 1000));
-                        
-                        /* build string of timer. */
-                        let timerString;
-                        // pad values with zeroes
-                        ms = ms.toString().padStart(3, '0');
-                        // When <1 min, exclude minutes 
-                        if (minutes < 1) {
-                            timerString = seconds + "." + ms;
-                        }
-                        else {
-                            minutes = minutes.toString().padStart(2, '0');
-                            seconds = seconds.toString().padStart(2, '0');
-                            timerString = minutes + ":" + seconds + "." + ms;
-                        }
-                        document.getElementById("timertext").innerHTML = timerString;
-                    }, TIMER_UPDATE_INTERVAL);
-                }
+                toggleTimer();
             }
         };
+    }
+
+    /**
+     * Turns the timer on or off. Does not use JS intervals or timeout for
+     * timekeeping; those are not guaranteed to be accurate. Instead uses
+     * JS Date().
+     */
+    function toggleTimer() {
+        if (timerRunning) {
+            // turn timer off
+            timerRunning = false;
+            clearInterval(timer);
+        }
+        else {
+            // turn timer on
+            timerRunning = true;
+
+            // get current time
+            timerStart = Date.now();
+
+            // set up interval to change timer text
+            timer = setInterval(function() {
+                // delta is ms since timer started
+                let delta = Date.now() - timerStart;
+                
+                // get values of numbers for parts of timer 
+                let minutes = Math.floor(delta / 60000);
+                let seconds = Math.floor(delta / 1000) % 60; // % 60 is for second rollover when > 1 minute
+                let ms = Math.floor(delta - (minutes * 60000) - (seconds * 1000));
+                
+                /* build string of timer. */
+                let timerString;
+                // pad values with zeroes
+                ms = ms.toString().padStart(3, '0');
+                // When <1 min, exclude minutes 
+                if (minutes < 1) {
+                    timerString = seconds + "." + ms;
+                }
+                else {
+                    minutes = minutes.toString().padStart(2, '0');
+                    seconds = seconds.toString().padStart(2, '0');
+                    timerString = minutes + ":" + seconds + "." + ms;
+                }
+                document.getElementById("timertext").innerHTML = timerString;
+            }, TIMER_UPDATE_INTERVAL);
+        }
     }
 })();
